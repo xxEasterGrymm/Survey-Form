ff59e52c6226702cf7bdc2ad08cebe345eefa3e8
xxEasterGrymm <aryabhattacharyya.study@gmail.com>
2021-07-02 02:12:07 +0530

    Survey 1 Form updated and php scripts added

--- public/php/survey1.php
+++ public/php/survey1.php
@@ -16,30 +16,31 @@
 	</nav>
 	<center>
 		<div id="containerdetails" class="container-fluid">
-			<div id="details" class="col-md-4 col-sm-6">
+			<div id="t1form" class="col-md-4 col-sm-6">
 				<?php
 				    $conn = mysqli_connect("localhost", "root", "", "test");
 				    // Check connection
 				        if($conn === false)die("ERROR: Could not connect. ". mysqli_connect_error());
 				        // Taking values from the form data(input)
-				        $name = $_REQUEST['name'];
-				        $age = $_REQUEST['age'];
-				        $address = $_REQUEST['address'];
-						$email = $_REQUEST['email'];
-						$gender = $_REQUEST['gender'];
-						$quali = $_REQUEST['quali'];
+                        $Q1 = $_REQUEST['Q1'];
+                        $arr = array($Q1);
+                        for($i = 1; $i < 10; $i++) {
+                            $x = $i + 1;
+                            $key = $_REQUEST["Q$x"];
+                            array_push($arr, $key);
+                        }
 				        // Performing insert query execution
-						if($quali == "Bachelor's Degree") $quali = "Bachelors Degree";
-						else if($quali == "Master's Degree") $quali = "Masters Degree";
-				       	$sql = "INSERT INTO `answers` (`name`, `age`, `address`, `Email ID`, `Gender`, `Educational Qualification`)
-						   		VALUES ('$name', '$age', '$address', '$email', '$gender', '$quali')";
-				       	if(mysqli_query($conn, $sql)) {
-				       		echo '<h3><font color="lightgreen">Your details have been recorded.</font> ✅</h3>';
-				      		echo nl2br("\nName - $name\n Age - $age\nAddress - $address\nEmail ID - $email\nGender - $gender\nEducational Qualification - $quali");
-				      	}
-				     else {
-				      		echo "ERROR: Sorry! $sql. ". mysqli_error($conn);
-					}
+                        for($i = 0; $i < 10; $i++) {
+                            $x = $i + 1;
+				       	    $sql = "INSERT INTO `survey` (`Survey Form`, `Question Number`, `Answer`) VALUES ('1', NULL , '$arr[$i]')";
+                            if(mysqli_query($conn, $sql)) {
+                                echo "Question $x - $arr[$i]<br/>";
+                            }
+                            else {
+                                echo "ERROR: Sorry! $sql. ". mysqli_error($conn);
+                            }
+                        }
+                        echo '<h3><font color="lightgreen">Your details have been recorded.</font> ✅</h3>';
 					    // Close connection
 					mysqli_close($conn);
 				?>
