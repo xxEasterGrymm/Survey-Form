eee5adb312d239d5185f7485aad74d83f46bfbff
SithLord34 <singh.shivansh2410@gmail.com>
2021-07-11 16:21:44 +0530

    initial commit

---
+++ public/js/about.js
@@ -0,0 +1,19 @@
+/*for smooth scroll*/
+$(document).ready(function(){
+    var scrollLink = $('.scroll');
+    scrollLink.click(function(e){
+        e.preventDefault();
+        $('body,html').animate({
+            scrollTop: $(this.hash).offset().top
+        }, 1000);
+    })
+})
+$(window).scroll(function() {
+    $("#text").css("opacity", 1 - $(window).scrollTop() / 250);
+});
+let text = document.getElementById('text');
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    text.style.top = 50 + value * -0.25 + '%';
+})
+
